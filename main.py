01150e0ac596747243f2562e5b660bc253fb9b93
TrofimovAssya <petitkalimero@gmail.com>
2020-06-17 15:34:13 -0400

    refactoring: adds the minibatch training

--- main.py
+++ main.py
@@ -11,6 +11,7 @@ import pickle
 import time
 import random
 import monitoring
+import training
 #
 def build_parser():
     parser = argparse.ArgumentParser(description="")
@@ -121,19 +122,18 @@ def main(argv=None):
 
 
             if opt.model == 'TCRonly':
-                inputs_s, inputs_k, targets = mini[0], mini[1], mini[2]
-                inputs_s = Variable(inputs_s, requires_grad=False).float()
-                inputs_k = Variable(inputs_k, requires_grad=False).float()
-                targets = Variable(targets, requires_grad=False).float()
-                if not opt.cpu:
-                    inputs_s = inputs_s.cuda(opt.gpu_selection)
-                    inputs_k = inputs_k.cuda(opt.gpu_selection)
-                    targets = targets.cuda(opt.gpu_selection)
-                inputs_k = inputs_k.squeeze().permute(0, 2, 1)
-                y_pred = my_model(inputs_k,inputs_s).float()
-                y_pred = y_pred.permute(1,0)
-
-
+                #inputs_s, inputs_k, targets = mini[0], mini[1], mini[2]
+                #inputs_s = Variable(inputs_s, requires_grad=False).float()
+                #inputs_k = Variable(inputs_k, requires_grad=False).float()
+                #targets = Variable(targets, requires_grad=False).float()
+                #if not opt.cpu:
+                #    inputs_s = inputs_s.cuda(opt.gpu_selection)
+                #    inputs_k = inputs_k.cuda(opt.gpu_selection)
+                #    targets = targets.cuda(opt.gpu_selection)
+                #inputs_k = inputs_k.squeeze().permute(0, 2, 1)
+                #y_pred = my_model(inputs_k,inputs_s).float()
+                #y_pred = y_pred.permute(1,0)
+                y_pred, my_model = training.TCRonly_batch(mini,opt,my_model)
                 loss = criterion(y_pred, targets)
                 if no_b % 5 == 0:
                     print (f"Doing epoch {t},examples{no_b}/{len(dataset)}.Loss:{loss.data.cpu().numpy().reshape(1,)[0]}")
@@ -145,39 +145,38 @@ def main(argv=None):
                 optimizer.zero_grad()
                 loss.backward()
                 optimizer.step()
-                #kmerembs = my_model.get_embeddings(inputs_k, inputs_s)[0].squeeze()
-                #np.save(f'{exp_dir}/kmer_embs/kmer_embs_batch_{no_b}',kmerembs.cpu().data.numpy())
 
 
             elif opt.model == 'allseq':
 
-                inputs_k, inputs_h1, inputs_h2, inputs_h3, inputs_h4, targets = mini[0], mini[1], mini[2], mini[3], mini[4], mini[5]
-                inputs_h1 = inputs_h1.repeat(inputs_k.shape[1],1,1)
-                inputs_h2 = inputs_h2.repeat(inputs_k.shape[1],1,1)
-                inputs_h3 = inputs_h3.repeat(inputs_k.shape[1],1,1)
-                inputs_h4 = inputs_h4.repeat(inputs_k.shape[1],1,1)
-                inputs_k = Variable(inputs_k, requires_grad=False).float()
-                targets = Variable(targets, requires_grad=False).float()
-                inputs_h1 = Variable(inputs_h1, requires_grad=False).float()
-                inputs_h2 = Variable(inputs_h2, requires_grad=False).float()
-                inputs_h3 = Variable(inputs_h3, requires_grad=False).float()
-                inputs_h4 = Variable(inputs_h3, requires_grad=False).float()
+                #inputs_k, inputs_h1, inputs_h2, inputs_h3, inputs_h4, targets = mini[0], mini[1], mini[2], mini[3], mini[4], mini[5]
+                #inputs_h1 = inputs_h1.repeat(inputs_k.shape[1],1,1)
+                #inputs_h2 = inputs_h2.repeat(inputs_k.shape[1],1,1)
+                #inputs_h3 = inputs_h3.repeat(inputs_k.shape[1],1,1)
+                #inputs_h4 = inputs_h4.repeat(inputs_k.shape[1],1,1)
+                #inputs_k = Variable(inputs_k, requires_grad=False).float()
+                #targets = Variable(targets, requires_grad=False).float()
+                #inputs_h1 = Variable(inputs_h1, requires_grad=False).float()
+                #inputs_h2 = Variable(inputs_h2, requires_grad=False).float()
+                #inputs_h3 = Variable(inputs_h3, requires_grad=False).float()
+                #inputs_h4 = Variable(inputs_h3, requires_grad=False).float()
 
-                if not opt.cpu:
-                    inputs_k = inputs_k.cuda(opt.gpu_selection)
-                    inputs_h1 = inputs_h1.cuda(opt.gpu_selection)
-                    inputs_h2 = inputs_h2.cuda(opt.gpu_selection)
-                    inputs_h3 = inputs_h3.cuda(opt.gpu_selection)
-                    inputs_h4 = inputs_h4.cuda(opt.gpu_selection)
-                    targets = targets.cuda(opt.gpu_selection)
-                inputs_k = inputs_k.squeeze().permute(0, 2, 1)
-                inputs_h1 = inputs_h1.squeeze().permute(0, 2, 1)
-                inputs_h2 = inputs_h2.squeeze().permute(0, 2, 1)
-                inputs_h3 = inputs_h3.squeeze().permute(0, 2, 1)
-                inputs_h4 = inputs_h4.squeeze().permute(0, 2, 1)
-                y_pred = my_model(inputs_k,inputs_h1, inputs_h2, inputs_h3,
-                                  inputs_h4).float()
-                y_pred = y_pred.permute(1,0)
+                #if not opt.cpu:
+                #    inputs_k = inputs_k.cuda(opt.gpu_selection)
+                #    inputs_h1 = inputs_h1.cuda(opt.gpu_selection)
+                #    inputs_h2 = inputs_h2.cuda(opt.gpu_selection)
+                #    inputs_h3 = inputs_h3.cuda(opt.gpu_selection)
+                #    inputs_h4 = inputs_h4.cuda(opt.gpu_selection)
+                #    targets = targets.cuda(opt.gpu_selection)
+                #inputs_k = inputs_k.squeeze().permute(0, 2, 1)
+                #inputs_h1 = inputs_h1.squeeze().permute(0, 2, 1)
+                #inputs_h2 = inputs_h2.squeeze().permute(0, 2, 1)
+                #inputs_h3 = inputs_h3.squeeze().permute(0, 2, 1)
+                #inputs_h4 = inputs_h4.squeeze().permute(0, 2, 1)
+                #y_pred = my_model(inputs_k,inputs_h1, inputs_h2, inputs_h3,
+                #                  inputs_h4).float()
+                #y_pred = y_pred.permute(1,0)
+                y_pred = training.allseq_batch(mini,opt,my_model)
                 if no_b == 10:
                     print ('******')
                     print ((y_pred.data.cpu().numpy()))
@@ -198,45 +197,46 @@ def main(argv=None):
 
             elif opt.model == 'allseq_bin':
 
-                inputs_k, inputs_h1, inputs_h2, inputs_h3, inputs_h4 = mini[0], mini[1], mini[2], mini[3], mini[4]
+                #inputs_k, inputs_h1, inputs_h2, inputs_h3, inputs_h4 = mini[0], mini[1], mini[2], mini[3], mini[4]
 
-                if inputs_h1.shape[1]>inputs_k.shape[1]:
-                    inputs_h1 = inputs_h1[:,:inputs_k.shape[1],:,:]
-                    inputs_h2 = inputs_h2[:,:inputs_k.shape[1],:,:]
-                    inputs_h3 = inputs_h3[:,:inputs_k.shape[1],:,:]
-                    inputs_h4 = inputs_h4[:,:inputs_k.shape[1],:,:]
-                elif inputs_h1.shape[1]>1:
-                    inputs_h1 = inputs_h1.repeat(inputs_k.shape[1],1,1)
-                    inputs_h2 = inputs_h2.repeat(inputs_k.shape[1],1,1)
-                    inputs_h3 = inputs_h3.repeat(inputs_k.shape[1],1,1)
-                    inputs_h4 = inputs_h4.repeat(inputs_k.shape[1],1,1)
-                inputs_k = Variable(inputs_k, requires_grad=False).float()
-                inputs_h1 = Variable(inputs_h1, requires_grad=False).float()
-                inputs_h2 = Variable(inputs_h2, requires_grad=False).float()
-                inputs_h3 = Variable(inputs_h3, requires_grad=False).float()
-                inputs_h4 = Variable(inputs_h3, requires_grad=False).float()
-                targets = np.zeros((inputs_k.shape[1],2))
-                size = int(inputs_k.shape[1]/2)
-                targets[:size,1]+=1
-                targets[size:,0]+=1
-                targets = torch.FloatTensor(targets)
-                targets = Variable(targets,requires_grad=False).float()
+                #if inputs_h1.shape[1]>inputs_k.shape[1]:
+                #    inputs_h1 = inputs_h1[:,:inputs_k.shape[1],:,:]
+                #    inputs_h2 = inputs_h2[:,:inputs_k.shape[1],:,:]
+                #    inputs_h3 = inputs_h3[:,:inputs_k.shape[1],:,:]
+                #    inputs_h4 = inputs_h4[:,:inputs_k.shape[1],:,:]
+                #elif inputs_h1.shape[1]>1:
+                #    inputs_h1 = inputs_h1.repeat(inputs_k.shape[1],1,1)
+                #    inputs_h2 = inputs_h2.repeat(inputs_k.shape[1],1,1)
+                #    inputs_h3 = inputs_h3.repeat(inputs_k.shape[1],1,1)
+                #    inputs_h4 = inputs_h4.repeat(inputs_k.shape[1],1,1)
+                #inputs_k = Variable(inputs_k, requires_grad=False).float()
+                #inputs_h1 = Variable(inputs_h1, requires_grad=False).float()
+                #inputs_h2 = Variable(inputs_h2, requires_grad=False).float()
+                #inputs_h3 = Variable(inputs_h3, requires_grad=False).float()
+                #inputs_h4 = Variable(inputs_h3, requires_grad=False).float()
+                #targets = np.zeros((inputs_k.shape[1],2))
+                #size = int(inputs_k.shape[1]/2)
+                #targets[:size,1]+=1
+                #targets[size:,0]+=1
+                #targets = torch.FloatTensor(targets)
+                #targets = Variable(targets,requires_grad=False).float()
 
-                if not opt.cpu:
-                    inputs_k = inputs_k.cuda(opt.gpu_selection)
-                    inputs_h1 = inputs_h1.cuda(opt.gpu_selection)
-                    inputs_h2 = inputs_h2.cuda(opt.gpu_selection)
-                    inputs_h3 = inputs_h3.cuda(opt.gpu_selection)
-                    inputs_h4 = inputs_h4.cuda(opt.gpu_selection)
-                    targets = targets.cuda(opt.gpu_selection)
-                inputs_k = inputs_k.squeeze().permute(0, 2, 1)
-                inputs_h1 = inputs_h1.squeeze().permute(0, 2, 1)
-                inputs_h2 = inputs_h2.squeeze().permute(0, 2, 1)
-                inputs_h3 = inputs_h3.squeeze().permute(0, 2, 1)
-                inputs_h4 = inputs_h4.squeeze().permute(0, 2, 1)
-                y_pred = my_model(inputs_k,inputs_h1, inputs_h2, inputs_h3,
-                                  inputs_h4).float()
-                y_pred = y_pred.permute(1,0)
+                #if not opt.cpu:
+                #    inputs_k = inputs_k.cuda(opt.gpu_selection)
+                #    inputs_h1 = inputs_h1.cuda(opt.gpu_selection)
+                #    inputs_h2 = inputs_h2.cuda(opt.gpu_selection)
+                #    inputs_h3 = inputs_h3.cuda(opt.gpu_selection)
+                #    inputs_h4 = inputs_h4.cuda(opt.gpu_selection)
+                #    targets = targets.cuda(opt.gpu_selection)
+                #inputs_k = inputs_k.squeeze().permute(0, 2, 1)
+                #inputs_h1 = inputs_h1.squeeze().permute(0, 2, 1)
+                #inputs_h2 = inputs_h2.squeeze().permute(0, 2, 1)
+                #inputs_h3 = inputs_h3.squeeze().permute(0, 2, 1)
+                #inputs_h4 = inputs_h4.squeeze().permute(0, 2, 1)
+                #y_pred = my_model(inputs_k,inputs_h1, inputs_h2, inputs_h3,
+                #                  inputs_h4).float()
+                y_pred = training.binallseq_batch(mini,opt,my_model)
+                #y_pred = y_pred.permute(1,0)
                 #targets = torch.reshape(targets,(targets.shape[0],1))
 
                 loss = criterion(y_pred, targets)
